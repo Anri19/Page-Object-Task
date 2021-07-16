dd7029b202b1ddd84c16859378de25949b3f1304
Anri19 <an_zakharov@mail.ru>
2021-07-16 16:51:56 +0300

    Changed test with should_be_login_page

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,12 +10,17 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in str(self.browser.current_url), "This is not login form"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not on this page"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not on this page"
+
+    # def go_to_login_page(self):
+    #     link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    #     link.click()
+    #     #return LoginPage(browser=self.browser, url=self.browser.current_url) 
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,73 @@
+from Pages.main_page import MainPage
+from Pages.login_page import LoginPage
+
+#link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    # page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page() # Здесь выполняются все тесты с login_page
+
+# def test_guest_should_be_login_url(browser):
+#     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#     page.open()                      # открываем страницу
+#  #   page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+#     page.should_be_login_url()
+
+# def test_guest_should_be_login_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#     page.open()                      # открываем страницу
+#  #   page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+#     page.should_be_login_form()
+    
+# def test_guest_should_be_register_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+#     page.open()                      # открываем страницу
+#  #   page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+#     page.should_be_register_form()  
+
+def test_guest_should_see_login_link(browser):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# def test_guest_should_be_login_url(broser):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
+
+# def test_guest_should_be_login_form(browser):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_form()
+
+# def test_guest_should_be_register_form(browser):
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_register_form()     
+
+#ПЕРВЫЙ ВАРИАНТ
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     browser.get(link)
+#     login_link = browser.find_element_by_css_selector("#login_link")
+#     login_link.click()
+
+#ВТОРОЙ ВАРИАНТ
+# def go_to_login_page(browser):
+#     login_link = browser.find_element_by_css_selector("#login_link")
+#     login_link.click()
+
+# def test_guest_can_go_to_login_page(browser): 
+#    browser.get(link) 
+#    go_to_login_page(browser) 
